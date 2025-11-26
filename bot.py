82220802fde91037b82d673600ad1cf88ce3b048
1danielusd1-crypto <1.daniel.usd.1@gmail.com>
2025-11-26 03:04:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2382,7 +2382,7 @@ def handle_text(msg):
                         #txt, _ = render_day_window(chat_id, day_key)
                         #kb = build_main_keyboard(day_key, chat_id)
                         #sent = bot.send_message(chat_id, txt, reply_markup=kb, parse_mode="HTML")
-                        update_or_end_day_window(chat_id, day_key)# текущее окно обновояет
+                        update_or_send_day_window(chat_id, day_key)# текущее окно обновояет
                         # запускаем таймер финальной логики (3 сек тишины)
                         schedule_finalize(chat_id, day_key)
                          # set_active_window_id(chat_id, day_key, sent.message_id)
